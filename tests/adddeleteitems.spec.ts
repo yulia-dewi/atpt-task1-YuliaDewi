81f46ef13222d5d0982e67b9161a5e9d58c2cf9f
yulia-dewi <yang.yuliadewi@gmail.com>
2025-06-19 22:40:33 +0700

    update 19062025 night

---
+++ tests/adddeleteitems.spec.ts
@@ -0,0 +1,77 @@
+import { test, expect } from '@playwright/test';
+
+test('add item per category and delete item - no login', async ({ page }) => {
+  await page.goto('https://demoblaze.com/');
+//   await page.getByRole('link', { name: 'Log in' }).click();
+//   await page.locator('#loginusername').fill('useryddeleteproduct');
+//   await page.locator('#loginpassword').fill('Pswd1234');
+//   await page.getByRole('button', { name: 'Log in' }).click();
+
+  const itemCategory = ['Phones', 'Laptops', 'Monitors'];
+  const categorizedItems = {
+    Phones: [
+      { name: 'Samsung galaxy s6', price: 360 },
+      { name: 'Nokia lumia 1520', price: 820 },
+      { name: 'Nexus 6', price: 650 },
+    //   { name: 'Samsung galaxy s7', price: 800 },
+    //   { name: 'Iphone 6 32gb', price: 790 },
+    //   { name: 'Sony xperia z5', price: 320 },
+    //   { name: 'HTC One M9', price: 700 }
+    ],
+    Laptops: [
+      { name: 'Sony vaio i5', price: 790 },
+    //   { name: 'Sony vaio i7', price: 790 },
+    //   { name: 'MacBook air', price: 700 },
+    //   { name: 'Dell i7 8gb', price: 700 },
+      { name: '2017 Dell 15.6 Inch', price: 700 },
+      // { name: 'MacBook Pro', price: 1100 }
+    ],
+    Monitors: [
+      { name: 'Apple monitor 24', price: 400 },
+      { name: 'ASUS Full HD', price: 230 }
+    ]
+  };
+
+  for (const category of itemCategory) {  //not adding all products because sometimes the pages not loading correctly
+  
+    for (const item of categorizedItems[category]) {
+      
+      await page.locator(`a.list-group-item`, { hasText: category }).click();
+      await page.locator('a', { hasText: item.name }).click();
+      
+      await Promise.all([
+        page.waitForEvent('dialog'),
+        await page.locator('//a[text()="Add to cart"]').click()
+      ]).then(async ([dialog]) => {
+        expect(dialog.message()).toContain('Product added');
+        await dialog.accept();
+      });
+  
+      await page.getByRole('link', { name: 'Home' }).click();
+      
+    }
+  }
+
+  await page.getByRole('link', { name: 'Cart' }).click();
+  await page.waitForTimeout(2000);
+
+  const productNameToDelete = 'Sony vaio i5';
+  const rows = await page.$$('.success');
+
+  for (const row of rows) {
+    const nameCell = await row.$('td:nth-child(2)');
+    const name = await nameCell?.innerText();
+
+    if (name?.trim() === productNameToDelete) {
+      const deleteLink = await row.$('a:text("Delete")');
+      if (deleteLink) {
+        await deleteLink.click();
+        console.log(`Deleted item: ${productNameToDelete}`);
+        break;
+      }
+    }
+  }
+ 
+  await expect(page.getByRole('cell', { name: 'Sony vaio i5' })).not.toBeVisible();
+
+});
--- tests/transactionp1p2.spec.ts
+++ tests/transactionp1p2.spec.ts
@@ -3,18 +3,25 @@ import { test, expect } from '@playwright/test';
 test('submit order success', async ({ page }) => {
   await page.goto('https://demoblaze.com/');
   await page.getByRole('link', { name: 'Log in' }).click();
-  await page.locator('#loginusername').fill('useryd');
+  await page.locator('#loginusername').fill('test1906');
   await page.locator('#loginpassword').fill('Pswd1234');
   await page.getByRole('button', { name: 'Log in' }).click();
 
   
-  const products = [
+  const products1 = [
     { name: 'Samsung galaxy s6', price: 360 },
     { name: 'Nokia lumia 1520', price: 820 },
     { name: 'HTC One M9', price: 700 }
   ];
+  const products2 = [
+    { name: 'Apple monitor 24', price: 400 },
+    { name: 'MacBook Pro', price: 1100 }
+  ];
+  let totalPrice=0;
 
-  for (const product of products) {
+  const allProducts = [...products1, ...products2];
+
+  for (const product of products1) {
     await page.locator(`//a[text()="${product.name}"]`).click();
     
 
@@ -28,18 +35,42 @@ test('submit order success', async ({ page }) => {
     expect(dialog.message()).toBe('Product added.');
     await dialog.accept();
 
+    totalPrice += product.price;
+
     // Go back to homepage for next product
     await page.getByRole('link', { name: 'Home' }).click();
     await page.waitForTimeout(1000); // wait for page to load
   }
 
+  for (const product of products2) {
+    await page.locator('//button[text()="Next"]').click();
+    await page.locator(`//a[text()="${product.name}"]`).click();
+    
+    // Prepare to catch the alert BEFORE clicking "Add to cart"
+    const dialogPromise = page.waitForEvent('dialog');
+
+    await page.locator('//a[text()="Add to cart"]').click();
+
+    const dialog = await dialogPromise;
+    console.log(dialog.message());
+    expect(dialog.message()).toBe('Product added.');
+    await dialog.accept();
+
+    totalPrice += product.price;
+
+    // Go back to homepage for next product
+    await page.getByRole('link', { name: 'Home' }).click();
+    await page.waitForTimeout(1000);
+  }
+
+
   // Go to Cart
   await page.getByRole('link', { name: 'Cart' }).click();
-  await page.waitForTimeout(2000); // allow cart table to render
+  await page.waitForTimeout(2000);
 
   // Check product names and prices in cart
   let totalCalculated = 0;
-  for (const product of products) {
+  for (const product of allProducts) {
     const row = page.locator(`//td[text()="${product.name}"]/..`);
     await expect(row.locator('td:nth-child(2)')).toHaveText(product.name);
     await expect(row.locator('td:nth-child(3)')).toHaveText(product.price.toString());
@@ -48,8 +79,8 @@ test('submit order success', async ({ page }) => {
 
   // Check total value
   const totalText = await page.locator('#totalp').innerText();
-  await expect(Number(totalText)).toBe(totalCalculated);
-  await expect(totalCalculated).toBe(1880);
+  expect(Number(totalText)).toBe(totalCalculated);
+  expect(totalCalculated).toBe(totalPrice);
   console.log(totalCalculated);
 
   // Click "Place Order"
@@ -71,7 +102,7 @@ test('submit order success', async ({ page }) => {
   console.log(formattedDate);
 
   await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
-  await expect(page.locator('p.lead')).toContainText(`Amount: ${totalCalculated} USD`);
+  await expect(page.locator('p.lead')).toContainText(`Amount: ${totalPrice} USD`);
   await expect(page.locator('p.lead')).toContainText('Card Number: 4525472847442424');
   await expect(page.locator('p.lead')).toContainText('Name: Safira');
   await expect(page.locator('p.lead')).toContainText(`Date: ${formattedDate}`); //demoblaze showing late 1 month, so getMonth not + 1
